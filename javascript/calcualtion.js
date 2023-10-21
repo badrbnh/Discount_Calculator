e841513a07101bcdc1bbf07d8f26a6b82aabcc56
badrbnh <badrechahide@gmail.com>
2023-10-21 20:16:17 +0100

    added limit digits

--- javascript/calcualtion.js
+++ javascript/calcualtion.js
@@ -19,3 +19,8 @@ function getValue()
         selectedElement.innerHTML = result.toFixed(2);
     }
 }
+function limitInput(input, maxLength) {
+    if (input.value.length > maxLength) {
+        input.value = input.value.slice(0, maxLength);
+    }
+}
